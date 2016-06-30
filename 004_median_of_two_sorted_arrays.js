7d8fb5e736e6f1354f5cd32cdb181076ff032029
Harri Feng <harrifeng@gmail.com>
2016-06-30 11:52:55 +0000

    new base 004

---
+++ 004_median_of_two_sorted_arrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    return 2.0;
+};
+
+var assert = require("assert");
+describe('Leetcode', function() {
+    it('004 median of two sorted arrays', function() {
+        assert.equal(2.0, findMedianSortedArrays([1, 3], [2]));
+    });
+});
